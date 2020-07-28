4a6b3d5e5cec3690e85a571ccf64656600b671c5
SaurabhLohani <68461646+saurabh643@users.noreply.github.com>
2020-07-28 14:10:34 +0530

    Add files via upload

---
+++ aliecegaming.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 28 13:52:28 2020
+
+@author: Saurabh
+"""
+
+print("WELCOME TO ALIENCE CLASH")
+print("\nhit one colour for below::")
+print("\ngreen,red")
+alien_colours=["green","red"]
+alien_colours=input()
+if 'green'in alien_colours:
+    print("You get 5 points:")
+else:
+    print("\nyou get 10 points")
+print("\nThanks for playing")
